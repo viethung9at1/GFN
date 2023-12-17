850cc7c72d6a7c0b36afa3de36d591d3974ef2d9
phuvinhnguyen <phuvinhnguyen812002@gmail.com>
2023-12-17 12:15:58 +0700

    update

--- src/models/backbone.py
+++ src/models/backbone.py
@@ -61,9 +61,6 @@ class Backbone(nn.Module):
 # Generic Head
 class Head(nn.Module):
     def forward(self, x) -> Dict[str, Tensor]:
-        print(x.shape)
-        print(self.head)
-        print('-'*60)
         feat = self.head(x)
         x = torch.amax(x, dim=(2, 3), keepdim=True)
         feat = torch.amax(feat, dim=(2, 3), keepdim=True)
@@ -102,9 +99,6 @@ class ConvnextBackbone(Backbone):
             convnext.features, return_layers=return_layers)
         self.out_channels = convnext.features[5][-1].block[5].out_features
 
-
-bonenum = 3
-
 # Convnext Head
 
 
@@ -122,94 +116,52 @@ class ConvnextHead(Head):
         self.featmap_names = ['feat_res4', 'feat_res5']
 
 
-class SwinBackbone(nn.Sequential):
-    def __init__(self, swin, out_channels=384):
+class permutei(torch.nn.Module):
+    def __init__(self):
         super().__init__()
-        self.swin = swin
-        self.out_channels = out_channels
 
     def forward(self, x):
-        semantic_weight = None
+        return x.permute(0, 2, 3, 1)
 
-        if self.swin.semantic_weight >= 0:
-            w = torch.ones(x.shape[0], 1) * self.swin.semantic_weight
-            w = torch.cat([w, 1-w], dim=-1)
-            semantic_weight = w.cuda()
 
-        x, hw_shape = self.swin.patch_embed(x)
+class permuteo(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
 
-        if self.swin.use_abs_pos_embed:
-            x = x + self.swin.absolute_pos_embed
-        x = self.swin.drop_after_pos(x)
+    def forward(self, x):
+        return x.permute(0, 3, 1, 2)
 
-        outs = []
-        for i, stage in enumerate(self.swin.stages[:bonenum]):
-            x, hw_shape, out, out_hw_shape = stage(x, hw_shape)
-            if self.swin.semantic_weight >= 0:
-                sw = self.swin.semantic_embed_w[i](
-                    semantic_weight).unsqueeze(1)
-                sb = self.swin.semantic_embed_b[i](
-                    semantic_weight).unsqueeze(1)
-                x = x * self.swin.softplus(sw) + sb
-            if i == bonenum-1:
-                norm_layer = getattr(self.swin, f'norm{i}')
-                out = norm_layer(out)
-                out = out.view(-1, *out_hw_shape,
-                               self.swin.num_features[i]).permute(0, 3, 1,
-                                                                  2).contiguous()
-                outs.append(out)
-        return outs[-1]
+
+class SwinBackbone(nn.Sequential):
+    def __init__(self, swin) -> None:
+        super().__init__()
+        return_layers = {
+            '5': 'feat_res4',
+        }
+        self.body = IntermediateLayerGetter(
+            swin.features, return_layers=return_layers
+        )
+        self.out_channels = swin.features[5][-1].mlp[3].out_features
 
 
 class SwinHead(nn.Sequential):
-    def __init__(self, swin, out_channels=384):
-        super().__init__()  # last block
-        self.swin = swin
-        self.out_channels = [out_channels, out_channels*2]
+    def __init__(self, swin):
+        super().__init__()
+        self.head = nn.Sequential(
+            permutei(),
+            swin.features[5],
+            swin.features[6],
+            swin.features[7],
+            permuteo(),
+        )
+        self.out_channels = [
+            swin.features[5][-1].mlp[3].out_features,
+            swin.features[7][-1].mlp[3].out_features,
+        ]
         self.featmap_names = ['feat_res4', 'feat_res5']
-
-    def forward(self, x):
-        semantic_weight = None
-        out = None
-
-        if self.swin.semantic_weight >= 0:
-            w = torch.ones(x.shape[0], 1) * self.swin.semantic_weight
-            w = torch.cat([w, 1-w], dim=-1)
-            semantic_weight = w.cuda()
-
-        feat = x
-        hw_shape = x.shape[-2:]
-        x = torch.flatten(x, 2)
-        x = x.permute(0, 2, 1)
-        x, hw_shape = self.swin.stages[bonenum-1].downsample(x, hw_shape)
-        if self.swin.semantic_weight >= 0:
-            sw = self.swin.semantic_embed_w[bonenum -
-                                            1](semantic_weight).unsqueeze(1)
-            sb = self.swin.semantic_embed_b[bonenum -
-                                            1](semantic_weight).unsqueeze(1)
-            x = x * self.swin.softplus(sw) + sb
-        for i, stage in enumerate(self.swin.stages[bonenum:]):
-            x, hw_shape, out, out_hw_shape = stage(x, hw_shape)
-            if self.swin.semantic_weight >= 0:
-                sw = self.swin.semantic_embed_w[bonenum +
-                                                i](semantic_weight).unsqueeze(1)
-                sb = self.swin.semantic_embed_b[bonenum +
-                                                i](semantic_weight).unsqueeze(1)
-                x = x * self.swin.softplus(sw) + sb
-            if i == len(self.swin.stages) - bonenum - 1:
-                norm_layer = getattr(self.swin, f'norm{bonenum+i}')
-                out = norm_layer(out)
-                out = out.view(-1, *out_hw_shape,
-                               self.swin.num_features[bonenum+i]).permute(0, 3, 1,
-                                                                          2).contiguous()
-        feat = self.swin.avgpool(feat)
-        out = self.swin.avgpool(out)
-        return {"feat_res4": feat, "feat_res5": out}
 
 
 # resnet model builder function
-
-
 def build_resnet(arch='resnet50', pretrained=True,
                  freeze_backbone_batchnorm=True, freeze_layer1=True,
                  norm_layer=misc_nn_ops.FrozenBatchNorm2d):
